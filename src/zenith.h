7469be97e7eb7d68389b04fd622cb06f7574c88c
Gauthier Picalausa <mc2m@uplinklabs.net>
2014-07-03 11:42:55 +0200

    Set up todo tasks

--- src/zenith.h
+++ src/zenith.h
@@ -2,6 +2,10 @@
 #ifndef _included_zenith_h
 #define _included_zenith_h
 
+#define ZENITH_NAME        "Zenith"
+#define ZENITH_DESCRIPTION "C platform for Lua IPC"
+#define ZENITH_VERSION     "0.5.0"
+
 #include "common.h"
 #include "state.h"
 
