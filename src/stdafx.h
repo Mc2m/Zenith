df79ea60d6050f5192f9c14e014e90103d411c75
Gauthier Picalausa <mc2m@uplinklabs.net>
2014-07-09 13:59:23 +0200

    Improve build
    Ensure that release builds properly
    Add custom property sheet path
    Clean stdafx

--- src/stdafx.h
+++ src/stdafx.h
@@ -2,25 +2,17 @@
 #ifndef __included_stdafx_h
 #define __included_stdafx_h
 
-#ifndef __STDC__
-#define inline _inline
-#endif
+#undef __cplusplus
 
-#ifdef _WINDOWS
+#if defined(_WIN32) || defined(_WIN64)
 #define _CRT_SECURE_NO_WARNINGS
 #pragma warning(disable : 4996)
-
-#define snprintf _snprintf
-
-#define __STDC__ 1 // Enforces ANSI C compliance.
-
-// __STDC__ disables the following definitions in the C headers
-#define strdup _strdup
-#define stricmp _stricmp
 #endif
 
 #include <stdlib.h>
 #include <stdio.h>
+
+#include <tdefine.h>
 
 #include <luajit.h>
 #include <lauxlib.h>
