d351380698e3129f0593683fc6a8b980b7758286
Gauthier Picalausa <mc2m@uplinklabs.net>
2014-07-14 01:56:16 +0200

    Fix Build and clean projects

--- contrib/build.bat
+++ contrib/build.bat
@@ -1,38 +1,103 @@
-@rem Script to build contribs with MSVC.
-@rem
+@echo off
 
-@if not defined INCLUDE goto :FAIL
+rem Script to build contribs with MSVC.
+rem
+setlocal
 
-@echo Building Zenith contribs
+set CONTRIBLIST=(luajit)
 
-@rem %1 is output path, %2 is debug or release, %3 is architecture
-@IF "%1"=="" goto :BAD
-@IF "%2"=="" goto :BAD
-@IF "%3"=="" goto :BAD
+IF "%1"=="clean" goto :CLEAN
 
-@rem fetch the contrib directory
-@set CBPATH=%~dp0
+if not defined INCLUDE goto :WRONG
 
-@rem fetch the directory where library should be put
-@set LIBPATH=%1
+rem %1 is output path, %2 is debug or release, %3 is architecture
+IF "%1"=="" goto :BAD
+IF "%2"=="" goto :BAD
+IF "%3"=="" goto :BAD
 
-@rem create the folder to be on the safe side
-@IF not exist %LIBPATH% @mkdir "%LIBPATH%"
+rem set the different parameters
+set SOLUTIONDIR=%1
+set CONFIGURATION=%2
+set PLATFORM=%3
 
-@cd %CBPATH%
+rem detect debug build
+set DEBUG=0
+if not x%CONFIGURATION:Debug=%==x%CONFIGURATION% (
+	set DEBUG=1
+	set CONFIGURATION=Debug
+) else (
+	if not x%CONFIGURATION:debug=%==x%CONFIGURATION% (
+		set DEBUG=1
+		set CONFIGURATION=Debug
+	)
+)
 
-@rem build lua jit
-@call "%CBPATH%luajit_build.bat" %LIBPATH% %2 %3
+rem fetch the toolset version from input.
+set TOOLSETVER=%VisualStudioVersion:.=%
 
+rem set output path
+set OUTPATH=%SOLUTIONDIR%lib\%PLATFORM%\%CONFIGURATION%\
 
-@IF errorlevel 0 @echo Build is successful
+rem create the folder to be on the safe side
+IF not exist %OUTPATH% mkdir "%OUTPATH%"
+
+rem fetch the contrib directory
+set CBPATH=%~dp0
+cd %CBPATH%s
+
+rem build contribs
+for %%i in %CONTRIBLIST% do (
+	call %%i_build.bat
+	IF errorlevel 1 goto :FAIL
+)
+
+endlocal
 goto :END
+
+:FAIL
+echo.
+echo *******************************************************
+echo *** Build FAILED -- Please check the error messages ***
+echo *******************************************************
+
+endlocal
+exit /b 1
+
+:CLEAN
+
+IF "%2"=="" goto :BAD
+IF "%3"=="" goto :BAD
+IF "%4"=="" goto :BAD
+
+rem set the different parameters
+set SOLUTIONDIR=%2
+set CONFIGURATION=%3
+set PLATFORM=%4
+
+rem set output path
+set OUTPATH=%SOLUTIONDIR%lib\%PLATFORM%\%CONFIGURATION%\
+
+rem fetch the contrib directory
+set CBPATH=%~dp0
+cd %CBPATH%
+
+echo Cleaning...
+for %%i in %CONTRIBLIST% do (
+	call %%i_build.bat clean
+)
+echo Done.
+
+endlocal
+exit /b 0
 
 :BAD
-@echo Missing library path
-@set ERRORLEVEL=1
-goto :END
-:FAIL
-@echo You must open a "Visual Studio .NET Command Prompt" to run this script
-@set ERRORLEVEL=1
+echo Missing library path
+endlocal
+exit /b 1
+
+:WRONG
+echo You must open a "Visual Studio .NET Command Prompt" to run this script
+endlocal
+exit /b 1
+
 :END
--- contrib/luajit_build.bat
+++ contrib/luajit_build.bat
@@ -1,32 +1,39 @@
-@rem Script to build luajit with MSVC.
-@rem
+rem Script to build luajit with MSVC.
+rem
 
-@IF "%1"=="" goto :BAD
+setlocal
 
-@set OUTPATH=%1
-@set OUTNAME=lua51.lib
+set PROJNAME=lua51
+set OUTPUT=%OUTPATH%%PROJNAME%.lib
 
-@IF exist %OUTPATH%%OUTNAME% (
-	@echo Nothing to do for LuaJIT
+IF "%1"=="clean" goto :CLEAN
+
+IF exist %OUTPUT% (
+	echo Nothing to do for LuaJIT
 	goto :END
 )
 
-@cd luajit/src
+echo === Building LuaJIT ===
+echo.
 
-@echo === Building LuaJIT ===
+IF %DEBUG%==1 set CONFIG=debug
 
-@setlocal
-call msvcbuild.bat %2 static
-@if errorlevel 0 (
-	@mv %OUTNAME% %LIBPATH%%OUTNAME%
-	@mv vc*.pdb %LIBPATH%
-	@del luajit.exe *.pdb luajit.ilk
+cd luajit/src
+call msvcbuild.bat %CONFIG% static
+if errorlevel 0 (
+	mv %PROJNAME%.lib %OUTPUT%
+	IF %DEBUG%==1 mv vc%TOOLSETVER%.pdb %OUTPATH%
+	del /F /Q luajit.exe *.pdb *.exp luajit.ilk
 )
-@endlocal
+cd ../..
 
-@cd ../..
+endlocal
 
-@goto :END
-:BAD
-@echo Missing parameter : output directory
+goto :END
+
+:CLEAN
+del /F /Q %OUTPUT% %OUTPATH%*.pdb
+endlocal
+goto :END
+
 :END
