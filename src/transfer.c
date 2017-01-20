6127b2dc1dc8517659a8dce00a11409281e42064
Gauthier Picalausa <mc2m@uplinklabs.net>
2017-01-20 17:32:45 +0100

    Transfer: improve code

--- src/transfer.c
+++ src/transfer.c
@@ -11,38 +11,44 @@ static int reftable[2] = {0};
 
 static void ZTransferDataInternal(lua_State *from, lua_State *to, int idx, int type);
 
-static int ZTransferFindMatch(lua_State *L, int reftable, const char *field, int idx) {
+static int ZTransferFindMatch(lua_State *L, int reftable, int idx) {
+	int found = 0;
+
 	if (reftable) {
-		int len, i = 1;
-		const void *ptr = lua_topointer(L, idx);
-		//maybe table exists
-		lua_getfield(L, reftable, field);
-
-		len = lua_objlen(L, -1);
-		for (; i <= len; ++i) {
-			lua_pushnumber(L, i);
-			lua_gettable(L, -2);
-			if (lua_istable(L, -1)) {
-				if (ptr == lua_topointer(L, -1)) {
-					lua_pop(L, 2);
-					return i;
-				}
-			}
-			lua_pop(L, 1);
-		}
-
+		lua_pushvalue(L, idx);
+		lua_rawget(L, reftable);
+		found = lua_tointeger(L, -1);
 		lua_pop(L, 1);
 	}
 
-	return 0;
+	return found;
 }
 
-static void ZTransferAddReference(lua_State *L, int idx, const char *field, int objidx) {
-	lua_getfield(L, idx, field);
-	lua_pushinteger(L, lua_objlen(L, -1) + 1);
+static int ZTransferAddReference(lua_State *L, int idx, int objidx, int index) {
+	if (!index) {
+		int i;
+		//get the index
+		lua_rawgeti(L, idx, 1);
+		i = lua_tointeger(L, -1);
+
+		//set the next index
+		lua_pushinteger(L, i + 1);
+		lua_rawseti(L, idx, 1);
+
+		//store the reference
+		lua_pushvalue(L, objidx);
+		lua_pushinteger(L, i);
+		lua_rawset(L, idx);
+
+		lua_pop(L, 1);
+
+		return i;
+	}
+
+	//store the reference
 	lua_pushvalue(L, objidx);
-	lua_settable(L, -3);
-	lua_pop(L, 1);
+	lua_rawseti(L, idx, index);
+	return 0;
 }
 
 static void ZTransferTable(lua_State *from, lua_State *to, int idx) {
@@ -51,32 +57,28 @@ static void ZTransferTable(lua_State *from, lua_State *to, int idx) {
 	if (!lua_checkstack(from, 2)) return;
 	if (!lua_checkstack(to, 2)) return;
 
-	if (idx < 0) idx += lua_gettop(from) + 1;
+	idx = L_INDEX_ABS(from, idx);
 
-	match = ZTransferFindMatch(from, reftable[0], "tables", idx);
+	match = ZTransferFindMatch(from, reftable[0], idx);
 	if (match) {
-		lua_getfield(to, reftable[1], "tables");
-		lua_pushinteger(to, match);
-		lua_gettable(to, -2);
-		lua_remove(to, -2);
+		lua_rawgeti(to, reftable[1], match);
 		return;
 	}
 
 	lua_newtable(to);
 
 	if (reftable[0]) {
-		ZTransferAddReference(from, reftable[0], "tables", idx);
-		ZTransferAddReference(to, reftable[1], "tables", lua_gettop(to));
+		int index = ZTransferAddReference(from, reftable[0], idx, 0);
+		ZTransferAddReference(to, reftable[1], lua_gettop(to), index);
 	}
 
 	lua_pushnil(from);
-
 	while (lua_next(from, idx)) {
 		/* uses 'key' (at index -2) and 'value' (at index -1) */
 		ZTransferDataInternal(from, to, -2, lua_type(from, -2));
 		ZTransferDataInternal(from, to, -1, lua_type(from, -1));
 
-		lua_settable(to, -3);
+		lua_rawset(to, -3);
 
 		lua_pop(from, 1);
 	}
@@ -104,14 +106,11 @@ static void ZTransferFunction(lua_State *from, lua_State *to, int idx) {
 		size_t len;
 		int match;
 
-		if (idx <= 0) idx += lua_gettop(from) + 1;
+		idx = L_INDEX_ABS(from, idx);
 
-		match = ZTransferFindMatch(from, reftable[0], "functions", idx);
+		match = ZTransferFindMatch(from, reftable[0], idx);
 		if (match) {
-			lua_getfield(to, reftable[1], "functions");
-			lua_pushinteger(to, match);
-			lua_gettable(to, -2);
-			lua_remove(to, -2);
+			lua_rawgeti(to, reftable[1], match);
 			return;
 		}
 
@@ -136,8 +135,8 @@ static void ZTransferFunction(lua_State *from, lua_State *to, int idx) {
 		lua_pop(from, 1);
 
 		if (reftable[0]) {
-			ZTransferAddReference(from, reftable[0], "functions", idx);
-			ZTransferAddReference(to, reftable[1], "functions", lua_gettop(to));
+			int index = ZTransferAddReference(from, reftable[0], idx, 0);
+			ZTransferAddReference(to, reftable[1], lua_gettop(to), index);
 		}
 	}
 }
@@ -152,14 +151,18 @@ static void ZTransferDataInternal(lua_State *from, lua_State *to, int idx, int t
 	else                                lua_pushnil(to);
 }
 
-static inline void ZTransferSetTransferTable(lua_State *L) {
-	lua_newtable(L);
+static inline void ZTransferSetTransferTable(lua_State *L, int source) {
+	if (source) {
+		//tracking table
+		lua_createtable(L, 1, 1);
 
-	lua_newtable(L);
-	lua_setfield(L, -2, "tables");
-
-	lua_newtable(L);
-	lua_setfield(L, -2, "functions");
+		//table tracking id
+		lua_pushnumber(L, 1);
+		lua_rawseti(L, -2, 1);
+	} else {
+		//tracking table
+		lua_createtable(L, 1, 0);
+	}
 }
 
 void ZTransferData(lua_State *from, lua_State *to, int idx) {
@@ -171,14 +174,16 @@ void ZTransferData(lua_State *from, lua_State *to, int idx) {
 	type = lua_type(from, idx);
 	if (type == LUA_TNONE) return;
 
-	if (idx < 0) idx += lua_gettop(from) + 1;
+	idx = L_INDEX_ABS(from, idx);
 
-	//We need to keep track of the elements in a separate table for complex elements
+	// We need to keep track of the elements in a separate table for complex elements
+	// in order to transfer loops (e.g. table1 { table2 { table1 }})
+
 	if (type == LUA_TTABLE) {
 		if (!lua_checkstack(to, 1)) return;
 		if (!lua_checkstack(to, 2)) return;
-		ZTransferSetTransferTable(from);
-		ZTransferSetTransferTable(to);
+		ZTransferSetTransferTable(from, 1);
+		ZTransferSetTransferTable(to, 0);
 		reftable[0] = lua_gettop(from);
 		reftable[1] = lua_gettop(to);
 	}
