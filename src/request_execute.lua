2b6b2bb7cd4c1e181d1a487b179c3571c758d55a
Gauthier Picalausa <mc2m@uplinklabs.net>
2017-02-21 14:14:11 +0100

    Request Execute: add function support in data processing

--- src/request_execute.lua
+++ src/request_execute.lua
@@ -10,9 +10,15 @@ function ExecuteRequest:new(content)
 end
 
 function ExecuteRequest:execute(data)
-    local f = loadstring(data[1])
-    table.remove(data, 1)
-    if type(f) == 'function' then return f(unpack(data)) end
+    if type(data[1]) == 'string' then
+        local f = loadstring(data[1])
+        table.remove(data, 1)
+        if f then return f(unpack(data)) end
+    elseif type(data[1]) == 'function' then
+        local f = data[1]
+        table.remove(data, 1)
+        return f(unpack(data))
+    end
 end
 
 return ExecuteRequest
